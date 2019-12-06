6295ae9243051737805662a7fddcaa7a57f1f47b
Alexandr Pogodin <awpogodin@mail.ru>
2019-12-07 00:42:45 +0400

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  date?: any;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'ng-todos';
+  appTitle = "Angular todo app";
+
+  public todos: Todo[] = [
+    { id: 1, title: "Купить хлеб", completed: false, date: new Date() },
+    { id: 2, title: "Купить масло", completed: true, date: new Date() },
+    { id: 3, title: "Купить пиво", completed: false, date: new Date() }
+  ];
+
+  onToggle(id: number) {
+    const idx = this.todos.findIndex(t => t.id === id);
+    this.todos[idx].completed = !this.todos[idx].completed;
+  }
 }
