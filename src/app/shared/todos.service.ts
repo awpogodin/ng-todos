7c278464b8ac19d2145b96944dd2c14763a20602
Alexandr Pogodin <awpogodin@mail.ru>
2019-12-07 16:38:18 +0400

    Update

--- src/app/shared/todos.service.ts
+++ src/app/shared/todos.service.ts
@@ -1,26 +1,37 @@
-import { Injectable } from "@angular/core";
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
 
 export interface Todo {
-  id: number;
-  title: string;
-  completed: boolean;
-  date?: any;
+    id: number;
+    title: string;
+    completed: boolean;
+    date?: any;
 }
 
-@Injectable({ providedIn: "root" })
+@Injectable({providedIn: 'root'})
 export class TodosService {
-  public todos: Todo[] = [
-    { id: 1, title: "Купить хлеб", completed: false, date: new Date() },
-    { id: 2, title: "Купить масло", completed: true, date: new Date() },
-    { id: 3, title: "Купить пиво", completed: false, date: new Date() }
-  ];
+    public todos: Todo[] = [];
 
-  onToggle(id: number) {
-    const idx = this.todos.findIndex(t => t.id === id);
-    this.todos[idx].completed = !this.todos[idx].completed;
-  }
+    constructor(private http: HttpClient) {
+    }
 
-  removeTodo(id: number) {
-    this.todos = this.todos.filter(t => t.id !== id);
-  }
+    fetchTodos(): Observable<Todo[]> {
+        return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=5')
+          .pipe(tap(todos => this.todos = todos));
+    }
+
+    onToggle(id: number) {
+        const idx = this.todos.findIndex(t => t.id === id);
+        this.todos[idx].completed = !this.todos[idx].completed;
+    }
+
+    removeTodo(id: number) {
+        this.todos = this.todos.filter(t => t.id !== id);
+    }
+
+    addTodo(todo: Todo) {
+        this.todos.push(todo);
+    }
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from "@angular/core";
-import { TodosService } from "../shared/todos.service";
+import {Component, OnInit} from '@angular/core';
+import {TodosService} from '../shared/todos.service';
+import {delay} from 'rxjs/operators';
 
 @Component({
-  selector: "app-todos",
-  templateUrl: "./todos.component.html",
-  styleUrls: ["./todos.component.scss"]
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
 })
 export class TodosComponent implements OnInit {
-  constructor(public todosService: TodosService) {}
 
-  ngOnInit() {}
+  private loading = true;
+  private searchString = '';
+
+  constructor(private todosService: TodosService) {
+  }
+
+  ngOnInit() {
+    this.todosService.fetchTodos()
+      .pipe(delay(500))
+      .subscribe(() => {
+        this.loading = false;
+      });
+  }
 
   onChange(id: number) {
     this.todosService.onToggle(id);
